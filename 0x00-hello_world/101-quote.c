b6c35a28f1767a3c3c4bbd08127dd507ed1dc473
Keven Mate <kevearmandomate@proton.me>
2022-12-08 21:33:44 +0000

    Solving

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
+	str = write(0,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 20);
 
 	return (1);
 }
