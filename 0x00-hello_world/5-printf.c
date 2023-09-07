8a8646d0229e8def2a6f2dcab10be2914fbad4b5
keven-mate <kevenarmandomate@proton.me>
2023-09-07 15:37:36 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main: Program that prints
+ * with proper grammar, but the outcome is a piece of art,
+ * using printf
+ *
+ * Return: Always (0)
+ */
+ int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
